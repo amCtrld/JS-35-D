bae41b5172ab94b6a9ab5ba1f980326ff5c8321a
amCtrld <petrembugua@proton.me>
2024-07-16 15:26:25 +0300

    Displays the user guessed number

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 const userGuessInput = document.getElementById("typeNo");
 const guessButton = document.getElementById("enteredNo");
 const resultDisplay = document.getElementById("result");
+const yourNo = document.getElementById("yourNo");
 
 // Define feedback messages
 const correctGuessMessage = "You guessed correct!";
@@ -15,6 +16,9 @@ const lowGuessMessage = "You guessed too low, try again!";
 guessButton.addEventListener("click", () => {
   // Get the user's guess as a number
   let guessedNumber = parseInt(userGuessInput.value);
+
+  // Display the user's guess
+  yourNo.textContent = `Your Guess: ${guessedNumber}`;
 
   // Provide feedback based on the guess
   if (guessedNumber === randomNumber) {
