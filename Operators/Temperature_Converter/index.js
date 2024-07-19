c75eb66929712d9604d8ec383ae38ee5d4efb039
amCtrld <petrembugua@proton.me>
2024-07-19 23:51:28 +0300

    C-F and Back | Converting both Celcius and Fahrenheit

--- Operators/Temperature_Converter/index.js
+++ Operators/Temperature_Converter/index.js
@@ -1,11 +1,22 @@
-const readTemp = document.getElementById("takeTemp");
-const sendTemp = document.getElementById("convertTemp");
-const postTemp = document.getElementById("result");
+const readTempInC = document.getElementById("takeTempInC");
+const sendTempInC = document.getElementById("convertTempToF");
+const postTempInF = document.getElementById("resultInF");
+
+const readTempInF = document.getElementById("takeTempInF");
+const sendTempInF = document.getElementById("convertTempToC");
+const postTempInC = document.getElementById("resultInC");
 
 
-sendTemp.addEventListener("click", () => {
-    let tempValue = readTemp.value;
+sendTempInC.addEventListener("click", () => {
+    let tempValue = readTempInC.value;
     tempValue = parseInt(tempValue);
     const fahrenheit = (tempValue * 9/2) + 35;
-    postTemp.textContent = fahrenheit;
+    postTempInF.textContent = fahrenheit;
+});
+
+sendTempInF.addEventListener("click", () => {
+    let tempValue = readTempInF.value;
+    tempValue = parseInt(tempValue);
+    const celcius = (tempValue * 9/2) + 35;
+    postTempInC.textContent = celcius;
 });
