a24246ddb60af00dab3e4aa6d35c225b1c606f3e
amCtrld <petrembugua@proton.me>
2024-07-21 20:30:45 +0300

    JS | added JS code for grading logic using if statements

--- Control_Flow/Grade_Calculator/index.js
+++ Control_Flow/Grade_Calculator/index.js
@@ -21,6 +21,20 @@ grade.addEventListener("click", () => {
     const name = studentName.value;
     const mark = studentMark.value;
 
-    if(mark )
+    if(mark >= 80 && mark <= 99) {
+        grade.innerHTML = "A";
+    } else if(mark >= 70 && mark <= 79) {
+        grade.innerHTML = "B";
+    } else if(mark >= 60 && mark <= 69) {
+        grade.innerHTML = "C";
+    } else if(mark >= 50 && mark <= 59) {
+        grade.innerHTML = "D";
+    } else if(mark >= 40 && mark <= 49) {
+        grade.innerHTML = "E";
+    } else {
+        grade.innerHTML = "F";
+    }
 
+    gradedName.innerHTML = name;
+    gradedMark.innerHTML = mark;
 });
