e1fe706305fac99fe236c756a75ddb8dddf0a497
amCtrld <petrembugua@proton.me>
2024-07-22 22:46:11 +0300

    Bitwise | Trying to use bitwise in the logic but all shows 'Odd'

--- Control_Flow/Even_Or_Odd_Checker/README.md
+++ Control_Flow/Even_Or_Odd_Checker/README.md
@@ -1,3 +1,4 @@
 # Project Description: Even or Odd Checker
 
 - Use conditional statements to classify numbers
+*Using both bitwise operation and modulo operator*
---
+++ Control_Flow/Even_Or_Odd_Checker/bitwise.js
@@ -0,0 +1,12 @@
+const number = document.getElementById("number");
+const button = document.getElementById("check");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+    let num = parseInt(number.value);
+    if (num & 1 === 0) {
+        result.innerHTML = "Even";
+    } else {
+        result.innerHTML = "Odd";
+    }
+});
