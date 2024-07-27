c6132caa6e4c3577903c55393b96f26e4a53d19d
amCtrld <petrembugua@proton.me>
2024-07-27 14:14:44 +0300

    Arrays | Creating, Accessing, Length, Adding to last, Adding to first, Removing last, Remocing first

---
+++ Arrays/ToDo_List/script.js
@@ -0,0 +1,18 @@
+//Creating arrays
+let fruitsList = ["apple", "banana", "orange"];
+console.log(fruitsList);
+//Accessing Arrays
+console.log(fruitsList[1]);
+//Length of array
+console.log(fruitsList.length);
+//Adding element to last
+fruitsList.push("mango");
+//Adding Element to first
+fruitsList.unshift("kiwi");
+console.log(fruitsList);
+//Removing last Element
+fruitsList.pop();
+console.log(fruitsList);
+//Removing first element
+fruitsList.shift();
+console.log(fruitsList);
