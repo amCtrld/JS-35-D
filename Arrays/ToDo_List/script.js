7474604f7cbc609008f45fe9ced90c649b7cb603
amCtrld <petrembugua@proton.me>
2024-07-28 15:40:39 +0300

    Added task priority feature

--- Arrays/ToDo_List/script.js
+++ Arrays/ToDo_List/script.js
@@ -3,6 +3,10 @@ let tasks = []; // Initialize an empty array to store tasks
 const addTaskButton = document.getElementById("addTask");
 const newTaskInput = document.getElementById("newTask");
 const taskList = document.getElementById("taskList");
+const highPriority = document.getElementById("high");
+const mediumPriority = document.getElementById("mid");
+const lowPriority = document.getElementById("low");
+
 
 // Retrieve tasks from session storage on page load
 const storedTasks = sessionStorage.getItem('todoList');
@@ -13,7 +17,13 @@ if (storedTasks) {
 addTaskButton.addEventListener("click", () => {
   const newTaskText = newTaskInput.value;
   if (newTaskText !== "") {
-    tasks.push(newTaskText);
+    let priority = "mid"; // Default priority
+    if (highPriority.checked) {
+      priority = "high";
+    } else if (lowPriority.checked) {
+      priority = "low";
+    }
+    tasks.push({ task: newTaskText, priority: priority });
     displayTasks();
     newTaskInput.value = "";
     saveToSessionStorage();
@@ -22,6 +32,22 @@ addTaskButton.addEventListener("click", () => {
 
 function displayTasks() {
   taskList.innerHTML = ""; // Clear the list before displaying tasks
+
+  // Sort tasks based on priority
+  tasks.sort((a, b) => {
+    if (a.priority === b.priority) {
+      return 0;
+    } else if (a.priority === "high") {
+      return -1;
+    } else if (b.priority === "high") {
+      return 1;
+    } else if (a.priority === "low") {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
   tasks.forEach((task, index) => {
     const taskItem = document.createElement("div");
     taskItem.classList.add(
@@ -32,7 +58,7 @@ function displayTasks() {
     );
 
     const taskText = document.createElement("span");
-    taskText.textContent = task;
+    taskText.textContent = task.task;
 
     // Create a container for the buttons
     const buttonContainer = document.createElement("div");
@@ -60,6 +86,18 @@ function displayTasks() {
       tasks.splice(index, 1);
       displayTasks();
     });
+
+    // Add priority icon
+    const priorityIcon = document.createElement("i");
+    priorityIcon.classList.add("fa", "fa-flag", "w3-margin");
+    if (task.priority === "high") {
+      priorityIcon.classList.add("w3-text-red");
+    } else if (task.priority === "mid") {
+      priorityIcon.classList.add("w3-text-yellow");
+    } else {
+      priorityIcon.classList.add("w3-text-green");
+    }
+    buttonContainer.appendChild(priorityIcon);
 
     // Append the buttons to the container
     buttonContainer.appendChild(doneButton);
