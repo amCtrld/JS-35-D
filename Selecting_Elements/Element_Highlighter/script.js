d9c48de5f7cc656576f30c262a5fe4cd14631d0a
amCtrld <petrembugua@proton.me>
2024-07-31 18:06:32 +0300

    Message Creation

--- Selecting_Elements/Element_Highlighter/script.js
+++ Selecting_Elements/Element_Highlighter/script.js
@@ -1,13 +1,17 @@
 const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
 const green = document.getElementById('green');
+const message = document.getElementById('message');
 
 red.addEventListener('click', () => {
     document.body.style.backgroundColor = 'red';
+    message.innerHTML = 'Red selected';
 });
 yellow.addEventListener('click', () => {
     document.body.style.backgroundColor = 'yellow';
+    message.innerHTML = 'Yellow selected';
 });
 green.addEventListener('click', () => {
     document.body.style.backgroundColor = 'green';
+    message.innerHTML = 'Green selected';
 });
