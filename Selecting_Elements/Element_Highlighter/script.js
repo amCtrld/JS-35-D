f11b36d9d9818f0f37b159b4bd4ce8c0e80d2f98
amCtrld <petrembugua@proton.me>
2024-08-01 13:59:33 +0300

    Change element color by id

--- Selecting_Elements/Element_Highlighter/script.js
+++ Selecting_Elements/Element_Highlighter/script.js
@@ -3,6 +3,27 @@ const sun = document.getElementById('sun');
 const moon = document.getElementById('moon');
 const body = document.querySelector('body');
 
+const red = document.getElementById('red');
+const yellow = document.getElementById('yellow');
+const green = document.getElementById('green');
+const message = document.getElementById('message');
+
+
+function changeColor(color) {
+    message.style.backgroundColor = color;
+    message.innerHTML = `The background color is now ${color}`;
+}
+
+red.addEventListener('click', () => {
+    changeColor('red');
+});
+yellow.addEventListener('click', () => {
+    changeColor('yellow');
+});
+green.addEventListener('click', () => {
+    changeColor('green');
+});
+
 toggle.addEventListener('click', () => {
     body.classList.toggle('w3-black');
     if (body.classList.contains('w3-black')) {
