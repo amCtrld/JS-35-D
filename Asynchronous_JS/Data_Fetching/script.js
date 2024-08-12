c0092922a38afe2cc5539569c5b3bf9bf57b7bd8
amCtrld <petrembugua@proton.me>
2024-08-12 20:46:15 +0300

    Encapsulating the code in a reusable function

--- Asynchronous_JS/Data_Fetching/script.js
+++ Asynchronous_JS/Data_Fetching/script.js
@@ -1,14 +1,26 @@
-const request = new XMLHttpRequest();
+const fetchData = () => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener(('readystatechange'),() => {
+  request.addEventListener("readystatechange", () => {
     //console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200){
-        console.log(request.responseText);
-    } else if(request.readyState === 4){
-        console.log('Request failed!');
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+    } else if (request.readyState === 4) {
+      console.log("Request failed!");
     }
-});
+  });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todosa/');
+  request.open("GET", "https://jsonplaceholder.typicode.com/todosa/");
 
-request.send();
+  request.send();
+};
+
+fetchData();
+fetchData();
+
+setTimeout(() => {
+  fetchData();
+}, 5000);
+
+fetchData();
+fetchData();
